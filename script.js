8b3ff105a260fe343999c02e9299d8e94019abff
theHighSain <sainandan.mohanty.sm@gmail.com>
2017-09-27 15:38:10 +0530

    add single node

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    frameRate();
+    initialise();
 }
 
 function draw() {
-    
-    
-    
+    background('#2c3e50');
+    node(xoff, yoff);
+    increment(0.01);  
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+
+function initialise() {
+    //initialises random offset values 
+    xoff = random(100);
+    yoff = random(100);
+}
+
+function node(xoff, yoff) {
+    //draws a node using noise function and offset values
+    x = noise(xoff) * width;
+    y = noise(yoff) * height;
+    
+    noStroke();
+    fill('#e74c3c');
+    ellipse(x, y, 10, 10);    
+}
+
+function increment(inc) {
+    //increments offset values
+    xoff += inc;
+    yoff += inc;
 }
