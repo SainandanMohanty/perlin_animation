89d2f094d03e1d9b834daee7c76c3304c29c24e1
theHighSain <sainandan.mohanty.sm@gmail.com>
2017-09-27 23:41:28 +0530

    remove scrollbars

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    windowDiagonal = dist(0, 0, width, height);
     frameRate();
     n = 32;
     initialise(n);
@@ -7,20 +8,20 @@ function setup() {
 
 function draw() {
     background(44, 62, 80);
-    
+
     calcPosition();
-    
-    for(i = 0; i< n; i++) {
-        for(j = (i + 1); j < n; j++) {
+
+    for (i = 0; i < n; i++) {
+        for (j = (i + 1); j < n; j++) {
             drawLink(i, j);
         }
     }
-    
-    for(i = 0; i < n; i++) {
+
+    for (i = 0; i < n; i++) {
         drawNode(arPos[i][0], arPos[i][1]);
     }
-    
-    increment(0.001);  
+
+    increment(0.001);
 }
 
 function windowResized() {
@@ -30,9 +31,9 @@ function windowResized() {
 function initialise() {
     //initialises an array of random offset values
     arOff = [];
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         arOff[i] = [];
-        for(j = 0; j < 2; j++) {
+        for (j = 0; j < 2; j++) {
             arOff[i][j] = random(1000);
         }
     }
@@ -41,19 +42,19 @@ function initialise() {
 function calcPosition() {
     //calculates co-ordinates using noise() and offset values from arOff[]
     arPos = [];
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         arPos[i] = [];
-        arPos[i][0] = map(noise(arOff[i][0]), 1/4, 3/4, 0, width);
-        arPos[i][1] = map(noise(arOff[i][1]), 1/4, 3/4, 0, height);    
+        arPos[i][0] = map(noise(arOff[i][0]), 1 / 4, 3 / 4, 0, width);
+        arPos[i][1] = map(noise(arOff[i][1]), 1 / 4, 3 / 4, 0, height);
     }
 }
 
 function drawLink(h, k) {
     //draws a link between two nodes
-    windowDiagonal = dist(0, 0, width, height);
+    //proximity and windowDiagonal help in scaling
     proximity = 1 / dist(arPos[h][0], arPos[h][1], arPos[k][0], arPos[k][1]);
-    
-    stroke(236, 240, 241, proximity * windowDiagonal);
+
+    stroke(236, 240, 241, proximity * windowDiagonal * 2);
     strokeWeight(min(proximity * windowDiagonal / 32, 16));
     line(arPos[h][0], arPos[h][1], arPos[k][0], arPos[k][1]);
 }
@@ -62,13 +63,13 @@ function drawNode(x, y) {
     //draws a node using co-ordinates from arPos[]
     noStroke();
     fill(231, 76, 60);
-    ellipse(x, y, 8, 8);    
+    ellipse(x, y, 8, 8);
 }
 
 function increment(inc) {
     //increments offset values
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < 2; j++) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < 2; j++) {
             arOff[i][j] += inc;
         }
     }
