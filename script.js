cb3d26f585a9f430559ed7c9118d932bbc9720ea
theHighSain <sainandan.mohanty.sm@gmail.com>
2017-09-27 17:41:10 +0530

    add links between nodes

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ function setup() {
 
 function draw() {
     background('#2c3e50');
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < 2; j++) {
-            drawNode(arOff[i][0], arOff[i][1]);
+    
+    calcPosition();
+    
+    for(i = 0; i< n; i++) {
+        for(j = 1; j < n; j++) {
+            drawLink(i, (i + j) % n);
         }
     }
-    increment(0.01);  
+    
+    for(i = 0; i < n; i++) {
+        drawNode(arPos[i][0], arPos[i][1]);
+    }
+    
+    increment(0.001);  
 }
 
 function windowResized() {
@@ -20,7 +28,7 @@ function windowResized() {
 }
 
 function initialise() {
-    //initialises random offset values
+    //initialises an array of random offset values
     arOff = [];
     for(i = 0; i < n; i++) {
         arOff[i] = [];
@@ -30,11 +38,25 @@ function initialise() {
     }
 }
 
-function drawNode(xoff, yoff) {
-    //draws a node using noise function and offset values
-    x = noise(xoff) * width;
-    y = noise(yoff) * height;
-    
+function calcPosition() {
+    //calculates co-ordinates using noise() and offset values from arOff[]
+    arPos = [];
+    for(i = 0; i < n; i++) {
+        arPos[i] = [];
+        arPos[i][0] = noise(arOff[i][0]) * width;
+        arPos[i][1] = noise(arOff[i][1]) * height;    
+    }
+}
+
+function drawLink(h, k) {
+    //draws a link between two nodes
+    stroke('#ecf0f1');
+    strokeWeight(0.5);
+    line(arPos[h][0], arPos[h][1], arPos[k][0], arPos[k][1]);
+}
+
+function drawNode(x, y) {
+    //draws a node using co-ordinates from arPos[]
     noStroke();
     fill('#e74c3c');
     ellipse(x, y, 10, 10);    
